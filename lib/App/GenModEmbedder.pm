fdb7af73e7d5499da76aea90c1e98c0d042111de
perlancar (@pc-home) <perlancar@gmail.com>
2016-12-26 09:03:10 +0700

    Release v0.001

--- lib/App/GenModEmbedder.pm
+++ lib/App/GenModEmbedder.pm
@@ -14,15 +14,27 @@ $SPEC{gen_mod_embedder} = {
     summary => 'Generate a piece of Perl code that embeds a module',
     description => <<'_',
 
-Suppose you depend on a (trivial, single file, stable) module and wants to
-eliminate dependency on that module by embedding it into your source code. Just
-put the output of this tool somewhere in your source code.
+Suppose your code depends on a (trivial, single file, stable) module and wants
+to eliminate dependency on that module by embedding it into your code. To do
+that, just put the output of this tool (the embedding code) somewhere in your
+source code. The structure of the embedding code is as follows:
 
-Compared to fatpacking, this technique does not use require hook and is suitable
-for use in module source code too in addition to script.
+    unless (eval { require Foo::Bar; 1 }) {
+        my $source = <<'END_OF_SOURCE';
+        ...
+        ...
+    END_OF_SOURCE
+        eval $source; die if $@;
+        $INC{'Foo/Bar.pm'} = '(set by ' . __FILE__ . ')';
+    }
 
-Compared to datapacking, this technique does not use require hook nor DATA
-section, suitable for use in module source code too in addition to script.
+Compared to fatpacking, this technique tries to load the original module first,
+does not use require hook, and is suitable for use inside .pm file as well as
+script.
+
+Compared to datapacking, this technique tries to load the original module first,
+does not use require hook nor DATA section, and is suitable for use inside .pm
+file as well as script.
 
 _
     args => {
@@ -35,7 +47,10 @@ _
             schema => ['bool*', is=>1],
             default => 1,
         },
-        # XXX indent_level
+        indent_level => {
+            schema => ['int*', min=>0],
+            default => 0,
+        },
     },
     links => [
         {url => 'Module::FatPack'},
@@ -45,6 +60,9 @@ _
     ],
 };
 sub gen_mod_embedder {
+    no strict 'refs';
+    no warnings 'once';
+    require ExtUtils::MakeMaker;
     require File::Slurper;
     require Module::Path::More;
 
@@ -55,29 +73,36 @@ sub gen_mod_embedder {
     my $path = Module::Path::More::module_path(module => $mod)
         or return [400, "Can't find module $mod on filesystem"];
 
+    my $version = MM->parse_version($path);
+    defined $version or return [400, "Can't extract VERSION for $mod from $path"];
+
     my $source = File::Slurper::read_text($path);
 
     if ($args{strip_pod}) {
         require Perl::Stripper;
-        my $stripper = Perl::Stripper->new(
-            strip_ws  => 0,
-            strip_pod => 1,
-            strip_comment => 0,
-        );
+        my $stripper = Perl::Stripper->new();
         $source = $stripper->strip($source);
     }
 
-    $source =~ s/^/#/g;
+    $source =~ s/\s+\z//s;
+    $source .= "\n";
+    $source =~ s/^/#/mg;
 
     # since literal \' and \\ inside single quote gets converted to literal '
     # and \, we need to escape the prefix \ to \\ in those cases.
     $source =~ s/\\(?='|\\)/\\\\$1/g;
 
-    my $preamble = "unless (eval { require $mod; 1 }) {\n";
-    $preamble .= "    eval '#line ' . __LINE__ . ' \"' . __FILE__ . qq(\"\\n) . <<'EOS';\n";
+    my $i0 = "    " x $args{indent_level};
+
+    my $preamble = "${i0}# BEGIN EMBEDDING MODULE: mod=$mod ver=$version generator=\"".__PACKAGE__." ".(${__PACKAGE__."::VERSION"})."\" generated-at=\"".(scalar localtime)."\"\n";
+    $preamble .= "${i0}unless (eval { require $mod; 1 }) {\n";
+    $preamble .= "${i0}    my \$source = '##line ' . (__LINE__+1) . ' \"' . __FILE__ . qq(\"\\n) . <<'EOS';\n";
     my $postamble = "EOS\n";
-    $postamble .= "    die if \$@;\n";
-    $postamble .= "}\n";
+    $postamble .= "${i0}    \$source =~ s/^#//gm;\n";
+    $postamble .= "${i0}    eval \$source; die if \$@;\n";
+    $postamble .= "${i0}    \$INC{'$mod_pm'} = '(set by embedding code in '.__FILE__.')';\n";
+    $postamble .= "${i0}}\n";
+    $postamble .= "${i0}# END EMBEDDING MODULE\n";
 
     return [200, "OK", $preamble . $source . $postamble,
             {"cmdline.skip_format" => 1}];
